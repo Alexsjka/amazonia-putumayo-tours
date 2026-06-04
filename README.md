c5208dfe362f70d026d96ad9c9ef57c108e648e8
Alexsjka <alejandromino903@gmail.com>
2026-06-04 14:54:30 -0500

    Proyecto inicial Amazonia Putumayo Tours

---
+++ README.md
@@ -0,0 +1,36 @@
+# Amazonía Putumayo Tours
+
+Sitio web turístico.
+
+## Tecnologías
+
+- HTML5
+- CSS3
+- JavaScript
+- Bootstrap 5
+- API Open-Meteo
+- LocalStorage
+
+## Funcionalidades
+
+- Sitio responsive
+- Formulario validado
+- Galería interactiva
+- Carrusel Bootstrap
+- Modales Bootstrap
+- Acordeones Bootstrap
+- Clima en tiempo real
+- Modo oscuro
+
+## Integrantes
+
+- Alejandro Miño
+- DarNelli Rivera
+
+## Repositorio
+
+- GitHub
+
+
+## Despliegue
+- versel
---
+++ js/script.js
@@ -0,0 +1,95 @@
+// =====================
+// API CLIMA
+// =====================
+
+const clima = document.getElementById("clima");
+
+if(clima){
+
+fetch("https://api.open-meteo.com/v1/forecast?latitude=1.1489&longitude=-76.6476&current_weather=true")
+
+.then(response=>response.json())
+
+.then(data=>{
+
+clima.innerHTML=`
+
+<h4> Clima Actual en Mocoa </h4>
+
+<p>Temperatura:
+${data.current_weather.temperature}°C</p>
+
+<p>Velocidad viento:
+${data.current_weather.windspeed} km/h</p>
+
+`;
+
+});
+
+}
+
+// =====================
+// MODO OSCURO
+// =====================
+
+function cambiarTema(){
+
+document.body.classList.toggle("dark-mode");
+
+localStorage.setItem(
+"tema",
+document.body.classList.contains("dark-mode")
+);
+
+}
+
+if(localStorage.getItem("tema")==="true"){
+
+document.body.classList.add("dark-mode");
+
+}
+
+// =====================
+// VALIDACIÓN FORMULARIO
+// =====================
+
+const formulario =
+document.getElementById("formulario");
+
+if(formulario){
+
+formulario.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+const nombre=
+document.getElementById("nombre").value.trim();
+
+const correo=
+document.getElementById("correo").value.trim();
+
+const mensaje=
+document.getElementById("mensaje").value.trim();
+
+if(nombre==="" ||
+correo==="" ||
+mensaje===""){
+
+alert("Complete todos los campos");
+
+return;
+
+}
+
+localStorage.setItem(
+"ultimoUsuario",
+nombre
+);
+
+alert("Formulario enviado correctamente");
+
+formulario.reset();
+
+});
+
+}
